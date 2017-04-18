2a53c8bea4fa6a45f8d46ce4f4e1b58525282aaa
GarenLiang <ideabreeze@gmail.com>
2017-04-18 16:43:28 -0700

    json web token

--- functions/verify_one_time_password.js
+++ functions/verify_one_time_password.js
@@ -11,11 +11,14 @@ module.exports = function(req, res) {
     .then(() => {
       const ref = admin.database().ref('users/' + phone);
       ref.on('value', snapshot => {
+        ref.off();
         const user = snapshot.val();
         if (user.code !== code || !user.codeValid) {
           return res.status(422).send({ error: 'Code not valid' });
         }
         ref.update({ codeValid: false });
+        admin.auth().createCustomToken(phone)
+          .then(token => res.send({ token: token }))
       });
     })
     .catch((err) => res.status(422).send({ error: err }))
